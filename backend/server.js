9fb852374d02249c8cbddb4f770d385f1564267d
Marine <jeanningros.marine@gmail.com>
2023-08-28 18:50:06 +0200

    backend done and homepage and form on frontend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
 require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 //creates an express app for us
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log('request: ', req.path, req.method)
     next()
 })
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('~#/.~#/.~#/.~ LISTENING on port', process.env.PORT,'~.#/~.#/~.#/~')
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('~#/.~#/.~#/.~ LISTENING on port', process.env.PORT,'~.#/~.#/~.#/~')
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
